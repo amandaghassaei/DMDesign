afff1d06db089aeeea6efe84dd7bc5bfed269c10
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-04-09 23:10:11 -0400

    laying out trox ui

--- js/menus/TroxUI.js
+++ js/menus/TroxUI.js
@@ -9,7 +9,9 @@ TroxUI = Backbone.View.extend({
 
     events: {
         "click .troxCellButton":                                       "_selectCellType",
-        "click #troxDeleteButton":                                     "_updateDeleteMode"
+        "click #troxDeleteButton":                                     "_updateDeleteMode",
+        "click .aboutTrox":                                            "_showAboutModal",
+        "click #troxReset":                                            "_showResetModal"
     },
 
     initialize: function(){
@@ -33,6 +35,16 @@ TroxUI = Backbone.View.extend({
         dmaGlobals.appState.set("deleteMode", !dmaGlobals.appState.get("deleteMode"));
     },
 
+    _showAboutModal: function(e){
+        e.preventDefault();
+
+    },
+
+    _showResetModal: function(e){
+        e.preventDefault();
+        
+    },
+
     render: function(){
         this.$el.html(this.template(_.extend(dmaGlobals.lattice.toJSON(), this.model.toJSON())));
     },
@@ -51,7 +63,10 @@ TroxUI = Backbone.View.extend({
               <a id="troxDeleteButton">\
                 <% if (deleteMode) { %><img src="assets/imgs/trox_highlighted_13.png">\
                 <% } else { %><img src="assets/imgs/trox_assets_13.png"><% } %></a>\
-              <a class="btn btn-primary">Reset</a>\
+        </div>\
+        <div id="troxFooter">\
+            <a class="aboutTrox pull-left" href="#"><img class="troxLogo" src="assets/imgs/trox_assets_15.png"></a>\
+            <a id="troxReset" href="#">Reset</a>\
         </div>\
         ')
 
