b8c48ae9d4e00c074fdab6d0393ddbc408b9675a
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-04-10 18:30:55 -0400

    texture/bmp finally working

--- js/fea/DMAPartTrox.js
+++ js/fea/DMAPartTrox.js
@@ -9,14 +9,17 @@
 
     var skin = THREE.ImageUtils.loadTexture('assets/textures/blackPaper.png');
 	skin.wrapS = skin.wrapT = THREE.RepeatWrapping;
+    skin.repeat.set(4,4);
 
-    var texture = THREE.ImageUtils.loadTexture( "assets/textures/blackPaper.png" );
+    var texture = THREE.ImageUtils.loadTexture( "assets/textures/paperSpecMap.jpg" );
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(4,4);
 
     var paperMaterial = new THREE.MeshPhongMaterial({
         map: skin,
-//        bumpMap:texture,
+        bumpMap:texture,
         color: new THREE.Color("#999999"),
-        bumpScale:10,
+        bumpScale:0.1,
         shininess:10,
         fog: false,
         shading:THREE.SmoothShading
@@ -33,6 +36,25 @@
         var unitScale = 1/3.25;
         tetraTrox.applyMatrix(new THREE.Matrix4().makeScale(unitScale, unitScale, unitScale));
         tetraTrox.applyMatrix(new THREE.Matrix4().makeRotationZ(Math.PI));
+        tetraTrox.computeBoundingBox();
+        var max = tetraTrox.boundingBox.max,
+            min = tetraTrox.boundingBox.min;
+        var offset = new THREE.Vector2(0 - min.x, 0 - min.y);
+        var range = new THREE.Vector2(max.x - min.x, max.y - min.y);
+        tetraTrox.faceVertexUvs[0] = [];
+        for (var i = 0; i < tetraTrox.faces.length ; i++) {
+
+            var v1 = tetraTrox.vertices[tetraTrox.faces[i].a], v2 = tetraTrox.vertices[tetraTrox.faces[i].b], v3 = tetraTrox.vertices[tetraTrox.faces[i].c];
+            tetraTrox.faceVertexUvs[0].push(
+                [
+                    new THREE.Vector2((v1.x + offset.x)/range.x ,(v1.y + offset.y)/range.y),
+                    new THREE.Vector2((v2.x + offset.x)/range.x ,(v2.y + offset.y)/range.y),
+                    new THREE.Vector2((v3.x + offset.x)/range.x ,(v3.y + offset.y)/range.y)
+                ]);
+
+        }
+        tetraTrox.uvsNeedUpdate = true;
+        tetraTrox.computeTangents();
     });
 
     function DMATetraTroxPart(type, parent){
@@ -41,7 +63,7 @@
     DMATetraTroxPart.prototype = Object.create(DMAPart.prototype);
 
     DMATetraTroxPart.prototype._makeMeshForType = function(){
-        var mesh = new THREE.Mesh(new THREE.SphereGeometry(1), paperMaterial);
+        var mesh = new THREE.Mesh(tetraTrox, paperMaterial);
         mesh.myPart = this;//need a ref back to this part
         return mesh;
     };
@@ -57,10 +79,26 @@
         octaTrox.computeBoundingBox();
         var unitScale = 1/3.25;
         octaTrox.applyMatrix(new THREE.Matrix4().makeScale(unitScale, unitScale, unitScale));
-        octaTrox.computeTangents();
-//        octaTrox = new THREE.Geometry().fromBufferGeometry(octaTrox);
+        octaTrox = new THREE.Geometry().fromBufferGeometry(octaTrox);
+        octaTrox.computeBoundingBox();
+        var max = octaTrox.boundingBox.max,
+            min = octaTrox.boundingBox.min;
+        var offset = new THREE.Vector2(0 - min.x, 0 - min.y);
+        var range = new THREE.Vector2(max.x - min.x, max.y - min.y);
+        octaTrox.faceVertexUvs[0] = [];
+        for (var i = 0; i < octaTrox.faces.length ; i++) {
 
-        console.log(octaTrox);
+            var v1 = octaTrox.vertices[octaTrox.faces[i].a], v2 = octaTrox.vertices[octaTrox.faces[i].b], v3 = octaTrox.vertices[octaTrox.faces[i].c];
+            octaTrox.faceVertexUvs[0].push(
+                [
+                    new THREE.Vector2((v1.x + offset.x)/range.x ,(v1.y + offset.y)/range.y),
+                    new THREE.Vector2((v2.x + offset.x)/range.x ,(v2.y + offset.y)/range.y),
+                    new THREE.Vector2((v3.x + offset.x)/range.x ,(v3.y + offset.y)/range.y)
+                ]);
+
+        }
+        octaTrox.uvsNeedUpdate = true;
+        octaTrox.computeTangents();
     });
 
     function DMAOctaTroxPart(type, parent){
